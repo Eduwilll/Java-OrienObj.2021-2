0145efc38ec2fb41dfe705749a6bbc29a2e3f01c
Eduwill <eduwill100@gmail.com>
2021-11-08 19:25:15 -0300

    first commit

--- Aula9/src/aula9/Aula9.java
+++ Aula9/src/aula9/Aula9.java
@@ -4,11 +4,16 @@ package aula9;
 public class Aula9 {
 
     public static void main(String[] args) {
-        
+        System.out.println("Dado");
         Dado d = new Dado();
         d.setAposta(6);
         d.jogaJogo();
         d.mostraRsultado();
+        System.out.println("Roleta");
+        Roleta r = new Roleta();
+        r.setAposta(21);
+        r.jogaJogo();
+        r.mostraRsultado();
     }
     
 }
--- Aula9/src/aula9/Roleta.java
+++ Aula9/src/aula9/Roleta.java
@@ -1,5 +1,23 @@
 package aula9;
 
+import java.util.Random;
+
 public class Roleta extends Jogo {
+    @Override
+    public void jogaJogo(){
+        Random rnd = new Random();
+        super.setResultado(rnd.nextInt(37));
+    }
+    @Override
+    public void setAposta(int aposta) {
+        if(aposta > 36 || aposta <0)
+             System.out.println("Valor inválido para o Roleta");
+        else
+            super.setAposta(aposta);
+    }
     
+     @Override
+    public void setResultado(int resultado) {
+        System.out.println("Ops, não pode roubar na roleta!!");
+    }
 }
